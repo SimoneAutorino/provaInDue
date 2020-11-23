d04b36692480ce1094451621916815973f521842
Marco I <m.imparato18@studenti.unisa.it>
2020-11-23 23:58:15 +0100

    Added a print

--- prova/src/App.java
+++ prova/src/App.java
@@ -3,5 +3,7 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("ciao");
         System.out.println("ciao sono Marco");
+        System.out.println("Bungee jumping");
     }
 }
+
