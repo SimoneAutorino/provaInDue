86cc4ec4289a10e3ef941e25043ee9e4afa47796
Marco I <m.imparato18@studenti.unisa.it>
2020-11-24 00:03:37 +0100

    Deleted prints

--- prova/src/App.java
+++ prova/src/App.java
@@ -1,10 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("ciao");
-        System.out.println("ciao sono Marco");
-        System.out.println("Bungee jumping");
-        System.out.println(" jumping");
+        
         System.out.println("Bungee");
         
     }
