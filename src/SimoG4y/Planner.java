bcebe90cd41717b46260c2ecb595b7a7b3fa95ff
marku <marku@Marco-1070>
2020-11-23 22:45:07 +0100

    Nuovo branch e classe Planner

---
+++ src/SimoG4y/Planner.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SimoG4y;
+
+/**
+ *
+ * @author marku
+ */
+public class Planner {
+    public String name;
+    public String pass;
+    private int id;
+    
+    public void doSomething(){
+    System.out.printl("Dodo scooby do");}
+    
+}
